1082dcb132f8e7f71e19ec5de7e1b4ed162f3c9f
Martin <martin.rostejnsky@email.cz>
2022-11-16 20:59:57 +0100

    dice

---
+++ TrpasliciPoker/Dice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrpasliciPoker
+{
+    internal class Dice
+    {
+        private int _Value;
+        private int _Size;
+        private bool _Locked;
+
+        public Dice(int sides)
+        {
+            _Size = sides;
+        }
+
+        public bool Roll()
+        {
+            if (!_Locked)
+            {
+                _Value = Random.Shared.Next(1,_Size);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int Value
+        {
+            get { return _Value; }
+        }
+    }
+}
